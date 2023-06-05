951ef8367fe24725df572b19ca30a0c6469f304a
Gio d'Amelio <giodamelio@gmail.com>
2023-06-05 17:02:47 +0000

    Switch to storing the url in Redis so that it survives fly restarts

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
@@ -8,10 +9,25 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/redis/go-redis/v9"
 )
 
 // Keep our string in memory
-var url string = ""
+var URL_KEY = "URL"
+
+func redisConnect() *redis.Client {
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		panic("REDIS_URL environment variable not set")
+	}
+
+	options, err := redis.ParseURL(redisURL)
+	if err != nil {
+		panic(err)
+	}
+
+	return redis.NewClient(options)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,13 +35,17 @@ func main() {
 		port = "8080"
 	}
 
-	r := chi.NewRouter()
+	// Connect to Redis Database
+	rdb := redisConnect()
 
+	// Setup Chi router
+	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	// Redirect to the URL
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		if url == "" {
+		url, err := rdb.Get(context.Background(), URL_KEY).Result()
+		if err != nil {
 			w.Write([]byte("No URL is currently set"))
 			return
 		}
@@ -35,7 +55,8 @@ func main() {
 
 	// See the url
 	r.Get("/see", func(w http.ResponseWriter, r *http.Request) {
-		if url == "" {
+		url, err := rdb.Get(context.Background(), URL_KEY).Result()
+		if err != nil {
 			w.Write([]byte("No URL is currently set"))
 			return
 		}
@@ -53,8 +74,15 @@ func main() {
 			return
 		}
 
-		url = string(body)
-		w.Write([]byte("URL has been set to " + url))
+		// Write the URL to Redis
+		err = rdb.Set(context.Background(), URL_KEY, body, 0).Err()
+		if err != nil {
+			w.Write([]byte("No URL is currently set"))
+			return
+		}
+
+		w.Write([]byte("URL has been set to "))
+		w.Write(body)
 	})
 
 	log.Printf("Starting server on port %s\n", port)
