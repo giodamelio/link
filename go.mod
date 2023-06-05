06ea8b238274102646897379ee760bf7b237b750
Gio d'Amelio <giodamelio@gmail.com>
2023-06-05 00:18:27 +0000

    Hello World http server with chi

---
+++ go.mod
@@ -0,0 +1,7 @@
+module link
+
+go 1.20
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
