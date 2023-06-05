e5f529487e2210747b31d22dc6333f1c854d1c7c
Gio d'Amelio <giodamelio@gmail.com>
2023-06-05 00:46:35 +0000

    Basic functionality complete

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -8,6 +9,9 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+// Keep our string in memory
+var url string = ""
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,8 +23,38 @@ func main() {
 
 	r.Use(middleware.Logger)
 
+	// Redirect to the URL
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		if url == "" {
+			w.Write([]byte("No URL is currently set"))
+			return
+		}
+
+		http.Redirect(w, r, url, http.StatusFound)
+	})
+
+	// See the url
+	r.Get("/see", func(w http.ResponseWriter, r *http.Request) {
+		if url == "" {
+			w.Write([]byte("No URL is currently set"))
+			return
+		}
+
+		w.Write([]byte(url))
+	})
+
+	// Set the URL
+	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		// Get the text from the body
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.Write([]byte("Could not set URL"))
+			w.WriteHeader(400)
+			return
+		}
+
+		url = string(body)
+		w.Write([]byte("URL has been set to " + url))
 	})
 
 	log.Printf("Starting server on port %s\n", port)
